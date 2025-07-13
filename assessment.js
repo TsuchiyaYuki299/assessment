0b1441aa63138a9fb91b92307fb8b0c67f91917c
TsuchiyaYuki299 <onigiri774@gmail.com>
2025-07-13 13:08:03 +0900

    ツイートボタンを追加

--- assessment.js
+++ assessment.js
@@ -22,10 +22,36 @@ assessmentButton.addEventListener(
     paragraph.innerText = result;
     resultDivision.appendChild(paragraph);
 
-    // TODO ツイートエリアの作成
+    //  ツイートエリアの作成
     tweetDivision.innerText = '';
+    const anchor = document.createElement('a');
+    const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag=' +
+                      encodeURIComponent('あなたのいいところ') +
+                      '&ref_src=twsrc%5Etfw';
+
+    anchor.setAttribute('href', hrefValue);
+    anchor.setAttribute('class', 'twitter-hashtag-button');
+    anchor.setAttribute('data-text', result);
+    anchor.innerText = 'Tweet #あなたのいいところ';
+
+    tweetDivision.appendChild(anchor);
+
+    const script = document.createElement('script');
+    script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
+    tweetDivision.appendChild(script);
   }
 );
+
+// TODO Enterキーで診断する処理を追加
+userNameInput.addEventListener(
+  'keydown',
+  (event) => {
+    if (event.code === 'Enter') {
+      assessmentButton.dispatchEvent(new Event('click'));
+      }
+  }
+)
+
 
 
 
